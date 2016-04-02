21fc423df068b62169d255cb4425dcfdd27bceb6
Attila Molnar <ate.molnar2@gmail.com>
2016-04-02 17:23:26 +0200

    User config, python console restarter
    Notes:
            User's custom config can be saved into a different file
            elpy starts automatically
            nodejs repl console
            function to restart ipython console from elpy to avoid annoying
            conflicts from reevaluation, like not reloading modules even when
            they are changed.

---
+++ user.el
@@ -0,0 +1,6 @@
+(setq user-full-name "Attila V. Molnar")
+(setenv "PYTHONPATH" "/home/attila/repo/python")
+
+;; Theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'monokai t)
