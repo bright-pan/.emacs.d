a5322fb421f2ee307952cf36af65893d0fa97e65
Attila Molnar <ate.molnar2@gmail.com>
2016-04-29 23:12:37 +0200

    kbd simplifying, Python path fix, yaml-mode
    Python path fix should be tested further

--- user.el
+++ user.el
@@ -1,7 +1,8 @@
 (setq user-full-name "Attila V. Molnar")
 (defvar user-home-dir (getenv "HOME") "Define home directory of the user")
-(setenv "PYTHONPATH" (concat user-home-dir "/repo/python"))
+(setenv "PYTHONPATH" (concat (getenv "PYTHONPATH") user-home-dir "/repo/python"))
 
 ;; Theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'monokai t)
+(toggle-frame-fullscreen)
